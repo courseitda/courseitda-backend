3327dc16ce51536e71731ca7dc1c9e29bc422acb
threepebbles <threepebbles@naver.com>
2025-08-15 22:39:00 +0900

    initial commit

---
+++ src/main/java/courseitda/CourseitdaApplication.java
@@ -0,0 +1,13 @@
+package courseitda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseitdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseitdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/courseitda/CourseitdaApplicationTests.java
@@ -0,0 +1,13 @@
+package courseitda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseitdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

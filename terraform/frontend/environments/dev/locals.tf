49ba3a09d31995d2eccaa29d78fa376f0865aca6
threepebbles <threepebbles@naver.com>
2025-10-06 21:54:17 +0900

    refactor: 프론트엔드 인프라 모듈화

--- terraform/frontend/environments/dev/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  environment = "dev"
-  fqdn        = "dev.courseitda.me"
-
-  tags = {
-    Project     = var.project_name
-    Environment = var.environment
-  }
-}
--- terraform/frontend/environments/dev/outputs.tf
+++ terraform/frontend/environments/dev/outputs.tf
@@ -2,41 +2,35 @@
 # Route53에서 발급 받은 것이 아닌, 외부 DNS이므로 레코드 수동 등록 필요
 # 아래 output 정보를 토대로 외부 DNS에 레코드 추가
 output "acm_dns_validation_records" {
-  value = [
-    for dvo in aws_acm_certificate.acm_certificate.domain_validation_options : {
-      name  = dvo.resource_record_name
-      type  = dvo.resource_record_type
-      value = dvo.resource_record_value
-    }
-  ]
+  value       = module.static_website.acm_dns_validation_records
   description = "Add the following CNAME records to your external DNS provider (e.g., Gabia). Continue after the certificate is issued."
 }
 
 output "bucket_name" {
-  value = aws_s3_bucket.bucket.bucket
+  value = module.static_website.bucket_name
 }
 
 output "cloudfront_domain" {
-  value = aws_cloudfront_distribution.cdn.domain_name
+  value = module.static_website.cloudfront_domain
 }
 
 output "cloudfront_id" {
-  value = aws_cloudfront_distribution.cdn.id
+  value = module.static_website.cloudfront_id
 }
 
 output "acm_certificate_arn" {
-  value       = aws_acm_certificate.acm_certificate.arn
+  value       = module.static_website.acm_certificate_arn
   description = "ACM Certificate ARN"
 }
 
 # ACM 발급 여부 확인을 위한 output
 output "acm_certificate_status" {
-  value       = aws_acm_certificate.acm_certificate.status
+  value       = module.static_website.acm_certificate_status
   description = "ACM Certificate Status"
 }
 
 # 외부 사용자 접속 URL
 output "website_url" {
-  value       = "https://${local.fqdn}"
+  value       = module.static_website.website_url
   description = "Web site URL"
 }

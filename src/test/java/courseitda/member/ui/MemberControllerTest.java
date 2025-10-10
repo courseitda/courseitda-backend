99432de1ed6ae3b73ff2d5fd5b961f4029efc91a
threepebbles <threepebbles@naver.com>
2025-10-10 20:02:37 +0900

    test: RestAssured를 이용한 회원가입 API E2E 테스트 추가

---
+++ src/test/java/courseitda/member/ui/MemberControllerTest.java
@@ -0,0 +1,59 @@
+package courseitda.member.ui;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import courseitda.member.domain.MemberFixture;
+import courseitda.member.ui.dto.request.SignUpRequest;
+import courseitda.member.ui.dto.response.SignUpResponse;
+import io.restassured.RestAssured;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+@Transactional
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class MemberControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    @DisplayName("회원가입에 성공한다")
+    void signup_success() {
+        // given
+        final String nickname = MemberFixture.anyNickname();
+        final String email = MemberFixture.anyEmail();
+        final String password = MemberFixture.anyPassword();
+        final SignUpRequest request = new SignUpRequest(nickname, email, password);
+
+        // when
+        final SignUpResponse response = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/members")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract()
+                .as(SignUpResponse.class);
+
+        // then
+        assertThat(response.id()).isNotNull();
+        assertThat(response.nickname()).isEqualTo(nickname);
+        assertThat(response.email()).isEqualTo(email);
+    }
+}

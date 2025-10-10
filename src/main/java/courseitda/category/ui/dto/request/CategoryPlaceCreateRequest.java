d7470dda338a4bdf9237a298ff99c8ca465e5683
RaZel713 <razel7130@gmail.com>
2025-10-10 20:17:39 +0900

    refactor: spotlessApply 적용

--- src/main/java/courseitda/category/ui/dto/request/CategoryPlaceCreateRequest.java
+++ src/main/java/courseitda/category/ui/dto/request/CategoryPlaceCreateRequest.java
@@ -6,5 +6,6 @@ public record CategoryPlaceCreateRequest(
         String addressName,
         double lat,
         double lng
+// todo: 검색 결과 상으로 도로명 주소의 유무에 따라 변경되어야 하는데, 해당 과정을 확인한 후 수정할 것
 ) {
 }

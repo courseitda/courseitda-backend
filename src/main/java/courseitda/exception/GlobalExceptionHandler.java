0b9440f4cf4e24c6763ac19af7bf8ed932d5d7d0
heiler <threepebbles@naver.com>
2025-10-10 14:46:08 +0900

    feat: 전역 예외 처리를 위한 GlobalExceptionHandler 추가 (#39)
    - ProblemDetail 사용

---
+++ src/main/java/courseitda/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package courseitda.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ProblemDetail handleException(Exception e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        problemDetail.setDetail(e.getMessage());
+        return problemDetail;
+    }
+}

678be497c4af1acdc762da8b665a613e0fc8a503
heiler <threepebbles@naver.com>
2025-10-17 20:29:31 +0900

    feat: 회원 닉네임 중복 검증 API 추가, 회원 이메일 중복 검증 API 추가 (#68)
    * feat: 회원 닉네임 중복 검증 API 추가, 회원 이메일 중복 검증 API 추가
    
    * refactor: MemberService 메서드 순서 변경
    
    * fix: MemberService에 닉네임, 이메일 검증 로직 추가

--- src/main/java/courseitda/member/application/MemberService.java
+++ src/main/java/courseitda/member/application/MemberService.java
@@ -6,6 +6,8 @@ import courseitda.common.exception.ErrorCode;
 import courseitda.member.domain.Member;
 import courseitda.member.domain.MemberRepository;
 import courseitda.member.ui.dto.request.SignUpRequest;
+import courseitda.member.ui.dto.response.CheckEmailDuplicateResponse;
+import courseitda.member.ui.dto.response.CheckNicknameDuplicateResponse;
 import courseitda.member.ui.dto.response.SignUpResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -38,6 +40,42 @@ public class MemberService {
     public Member findById(final Long id) {
         return memberRepository.findById(id)
                 .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
+    }
+
+    public CheckNicknameDuplicateResponse checkNicknameDuplicate(final String nickname) {
+        validateNicknameNotEmpty(nickname);
+
+        final boolean isDuplicated = memberRepository.existsByNickname(nickname);
+
+        return new CheckNicknameDuplicateResponse(isDuplicated);
+    }
+
+    public CheckEmailDuplicateResponse isEmailDuplicate(final String email) {
+        validateEmailNotEmpty(email);
+        validateEmailFormat(email);
+
+        final boolean isDuplicated = memberRepository.existsByEmail(email);
+
+        return new CheckEmailDuplicateResponse(isDuplicated);
+    }
+
+    private void validateNicknameNotEmpty(final String nickname) {
+        if (nickname == null || nickname.isBlank()) {
+            throw new BusinessException(ErrorCode.MEMBER_NICKNAME_EMPTY);
+        }
+    }
+
+    private void validateEmailNotEmpty(final String email) {
+        if (email == null || email.isBlank()) {
+            throw new BusinessException(ErrorCode.MEMBER_EMAIL_EMPTY);
+        }
+    }
+
+    private void validateEmailFormat(final String email) {
+        final String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        if (!email.matches(emailRegex)) {
+            throw new BusinessException(ErrorCode.INVALID_EMAIL_FORMAT);
+        }
     }
 
     private void validateDuplicateEmail(final String email) {

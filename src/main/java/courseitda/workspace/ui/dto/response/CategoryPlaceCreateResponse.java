ac49c3b77d66021086370af495f3ef981aaec8e8
RaZel713 <razel7130@gmail.com>
2025-10-22 17:58:32 +0900

    feat: 카테고리 장소 생성 응답 객체 수정
    - 장소 정보를 포함하도록 응답 객체 변경
    - 장소 ID, 이름, 도로 주소, 일반 주소, 위도, 경도 추가

--- src/main/java/courseitda/workspace/ui/dto/response/CategoryPlaceCreateResponse.java
+++ src/main/java/courseitda/workspace/ui/dto/response/CategoryPlaceCreateResponse.java
@@ -4,13 +4,23 @@ import courseitda.workspace.application.dto.response.CreateCategoryPlaceResult;
 
 public record CategoryPlaceCreateResponse(
         Long id,
-        Long placeId
+        Long placeId,
+        String name,
+        String roadAddressName,
+        String addressName,
+        double latitude,
+        double longitude
 ) {
 
     public static CategoryPlaceCreateResponse from(final CreateCategoryPlaceResult result) {
         return new CategoryPlaceCreateResponse(
                 result.id(),
-                result.placeId()
+                result.place().getId(),
+                result.place().getName(),
+                result.place().getRoadAddressName(),
+                result.place().getAddressName(),
+                result.place().getLatitude(),
+                result.place().getLongitude()
         );
     }
 }

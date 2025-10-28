2993b06c66b1848dad5a7d7df147af156d0403f7
heiler <threepebbles@naver.com>
2025-10-28 17:55:48 +0900

    feat: 요청 DTO 검증에 대한 예외 처리 기능 추가 (#81)
    * refactor: 변수명 일관되게 수정(isDuplicate -> isDuplicated)
    
    * feat: 요청 DTO 검증에 대한 예외 처리 기능 추가

--- src/main/java/courseitda/common/exception/GlobalExceptionHandler.java
+++ src/main/java/courseitda/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package courseitda.common.exception;
 
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,5 +27,37 @@ public class GlobalExceptionHandler {
         return e.getErrorCode().toProblemDetail();
     }
 
-    // todo: 요청 DTO 검증 실패 예외 핸들러 추가
+    /*
+     * 요청 DTO에서 validation 어노테이션 검증에 실패했을 때 동작하는 예외 처리기입니다.
+     * 응답 JSON 예시
+     * {
+     *     "type": "about:blank",
+     *     "title": "Bad Request",
+     *     "status": 400,
+     *     "detail": "요청 데이터 검증에 실패했습니다.",
+     *     "code": "0001",
+     *     "fieldErrors": {
+     *         "nickname": "공백일 수 없습니다",
+     *         "email": "공백일 수 없습니다",
+     *         "password": "비밀번호는 6자 이상 20자 이하이어야 합니다."
+     *     }
+     *  }
+     * */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ProblemDetail handleValidationException(final MethodArgumentNotValidException e) {
+        final ProblemDetail problemDetail = ErrorCode.REQUEST_VALIDATION_FAILED.toProblemDetail();
+
+        final Map<String, String> fieldErrors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        error -> Objects.requireNonNullElse(error.getDefaultMessage(), "유효하지 않은 값입니다."),
+                        (existing, replacement) -> existing
+                ));
+
+        problemDetail.setProperty("fieldErrors", fieldErrors);
+        return problemDetail;
+    }
 }

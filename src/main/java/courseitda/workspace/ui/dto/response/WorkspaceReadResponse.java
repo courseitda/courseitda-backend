305ccb8955bf5342340cc8cc24bf0abd02f6b4c9
threepebbles <threepebbles@naver.com>
2025-10-30 14:26:10 +0900

    refactor: 네이밍 컨벤션에 맞게 DTO 네이밍 수정

--- src/main/java/courseitda/workspace/ui/dto/response/WorkspaceReadResponse.java
+++ src/main/java/courseitda/workspace/ui/dto/response/WorkspaceReadResponse.java
@@ -4,14 +4,14 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import courseitda.workspace.application.dto.response.FindWorkspaceResult;
 import java.time.LocalDateTime;
 
-public record WorkspaceFindResponse(
+public record WorkspaceReadResponse(
         String identifier,
         String title,
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime modifiedAt
 ) {
 
-    public static WorkspaceFindResponse from(final FindWorkspaceResult result) {
-        return new WorkspaceFindResponse(
+    public static WorkspaceReadResponse from(final FindWorkspaceResult result) {
+        return new WorkspaceReadResponse(
                 result.identifier(),
                 result.title(),
                 result.modifiedAt()

fdf8186ea432902d5fff9d19acc7b528aa43e5b5
RaZel713 <razel7130@gmail.com>
2025-11-16 01:17:49 +0900

    feat: CategoryPlaceRepository에 워크스페이스 삭제 관련 메서드 추가

--- src/main/java/courseitda/workspace/infrastructure/JpaCategoryPlaceRepository.java
+++ src/main/java/courseitda/workspace/infrastructure/JpaCategoryPlaceRepository.java
@@ -1,7 +1,10 @@
 package courseitda.workspace.infrastructure;
 
 import courseitda.workspace.domain.CategoryPlace;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaCategoryPlaceRepository extends JpaRepository<CategoryPlace, Long> {
+
+    void deleteAllByCategoryIdIn(List<Long> categoryIds);
 }

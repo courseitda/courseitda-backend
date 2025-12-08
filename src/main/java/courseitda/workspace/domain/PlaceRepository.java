afecc9355561a13db27a89f0773352432d569e89
RaZel713 <razel7130@gmail.com>
2025-12-08 21:55:45 +0900

    feat: PlaceRepository에 삭제 메서드 추가

--- src/main/java/courseitda/workspace/domain/PlaceRepository.java
+++ src/main/java/courseitda/workspace/domain/PlaceRepository.java
@@ -1,6 +1,12 @@
 package courseitda.workspace.domain;
 
+import java.util.List;
+
 public interface PlaceRepository {
 
     Place save(Place place);
+
+    void deleteById(Long placeId);
+
+    void deleteAllByIds(List<Long> placeIds);
 }
--- src/main/java/courseitda/workspace/infrastructure/PlaceRepositoryImpl.java
+++ src/main/java/courseitda/workspace/infrastructure/PlaceRepositoryImpl.java
@@ -2,6 +2,7 @@ package courseitda.workspace.infrastructure;
 
 import courseitda.workspace.domain.Place;
 import courseitda.workspace.domain.PlaceRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +15,17 @@ public class PlaceRepositoryImpl implements PlaceRepository {
     @Override
     public Place save(final Place place) {
         return jpaPlaceRepository.save(place);
+    }
+
+    @Override
+    public void deleteById(final Long placeId) {
+        jpaPlaceRepository.deleteById(placeId);
+    }
+
+    @Override
+    public void deleteAllByIds(final List<Long> placeIds) {
+        if (!placeIds.isEmpty()) {
+            jpaPlaceRepository.deleteAllById(placeIds);
+        }
     }
 }
